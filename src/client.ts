23be55dfdff05352496226279b14e3ddd5933ce5
Vojtěch Döme <ojta@post.cz>
2023-01-20 09:32:52 +0100

    1.0

--- src/client.ts
+++ src/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  "https://odwqblaacvgrzpvdgcdr.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9kd3FibGFhY3ZncnpwdmRnY2RyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQwNzg0NzUsImV4cCI6MTk4OTY1NDQ3NX0.IFtIX-PMiGJ0EMB-yyXwLM6K7u13fBMUMlNQj44stWI"
+  " ",
+  " "
 );
 
 export { supabase };
